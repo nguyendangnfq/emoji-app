931657b5e1b320d5956e507cbf2caa7b3f32b080
nguyendangnfq <nguyen.dang@nfq.asia>
2022-10-10 11:17:22 +0700

    feat: add async-storage features

--- src/App.provider.tsx
+++ src/App.provider.tsx
@@ -1,10 +1,15 @@
-import React, { useContext } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { useContext, useEffect } from 'react';
 import { createContext, useState, useCallback } from 'react';
 import { MoodOptionType, MoodOptionWithTimeStamp } from './types';
 
 type AppContextType = {
   moodList: MoodOptionWithTimeStamp[];
   handleSelectMood: (mood: MoodOptionType) => void;
+};
+
+type AppData = {
+  moods: MoodOptionWithTimeStamp[];
 };
 
 type ProviderProps = {
@@ -16,16 +21,52 @@ const defaultValue = {
   handleSelectMood: () => {},
 };
 
+const storageKey = 'my-app-data';
+
+const getAppData = async (): Promise<AppData | null> => {
+  try {
+    const data = await AsyncStorage.getItem(storageKey);
+
+    if (data) {
+      return JSON.parse(data);
+    }
+  } catch (error) {
+    return null;
+  }
+
+  return null;
+};
+
+const setAppData = async (newData: AppData) => {
+  try {
+    await AsyncStorage.setItem(storageKey, JSON.stringify(newData));
+  } catch {}
+};
+
 const AppContext = createContext<AppContextType>(defaultValue);
 
 export const AppProvider: React.FC<ProviderProps> = props => {
   const [moodList, setMoodList] = useState<MoodOptionWithTimeStamp[]>([]);
 
-  const handleSelectMood = useCallback((selectedMood: MoodOptionType) => {
-    setMoodList(current => [
-      ...current,
-      { mood: selectedMood, timestamp: Date.now() },
-    ]);
+  useEffect(() => {
+    const getDataFromStorage = async () => {
+      const data = await getAppData();
+
+      console.log(data);
+
+      if (data) {
+        setMoodList(data.moods);
+      }
+    };
+    getDataFromStorage();
+  }, []);
+
+  const handleSelectMood = useCallback((mood: MoodOptionType) => {
+    setMoodList(current => {
+      const newValue = [...current, { mood, timestamp: Date.now() }];
+      setAppData({ moods: newValue });
+      return newValue;
+    });
   }, []);
   return (
     <AppContext.Provider value={{ moodList, handleSelectMood }}>
