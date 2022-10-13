ce55b9ffd48fd6e97f0046df3946505ea427c23c
nguyendangnfq <nguyen.dang@nfq.asia>
2022-10-13 16:47:40 +0700

    feat: add chart and done finish tutorial

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import { BottomTabsNavigator } from './screens/BottomTabs.navigator';
 import { AppProvider } from './App.provider';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export const App: React.FC = () => {
   return (
     <AppProvider>
-      <NavigationContainer>
-        <BottomTabsNavigator />
-      </NavigationContainer>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <NavigationContainer>
+          <BottomTabsNavigator />
+        </NavigationContainer>
+      </GestureHandlerRootView>
     </AppProvider>
   );
 };
--- src/screens/Analytics.screen.tsx
+++ src/screens/Analytics.screen.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { useAppContext } from '../App.provider';
+import { VictoryPie } from 'victory-native';
+import _ from 'lodash';
 
 export const Analytics: React.FC = () => {
+  const appContext = useAppContext();
+  const data = Object.entries(_.groupBy(appContext.moodList, 'mood.emoji')).map(
+    ([key, value]: any) => ({
+      x: key,
+      y: value.length,
+    }),
+  );
+
   return (
     <View style={styles.container}>
-      <Text>Analytics</Text>
+      <VictoryPie
+        data={data}
+        colorScale={['tomato', 'orange', 'gold', 'cyan', 'navy']}
+        innerRadius={50}
+        labelRadius={({ innerRadius }) => innerRadius + 20}
+        style={{ labels: { fontSize: 30 } }}
+      />
     </View>
   );
 };
